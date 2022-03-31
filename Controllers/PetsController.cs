44ada3231d3e3c47f1259112c131e2b932ee6098
Brant McCarthy <brant.mccarthy@me.com>
2022-03-31 12:03:12 -0500

    controller in progress

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -19,12 +19,84 @@ namespace pet_hotel.Controllers
             _context = context;
         }
 
+        // --- Get All Pets ----------------------------------------------------
+        [HttpGet] 
+        public IEnumerable<Pet> GetPets() 
+        {
+            Console.WriteLine("Get All Pets!");
+            return _context.Pets;
+        }
+
+        // --- Get Pet by ID ---------------------------------------------------
+        [HttpGet("{id}")]
+        public ActionResult<PetOwner> GetById(int id) 
+        {
+            Console.WriteLine("get one pet");
+            PetOwner pet = _context.Pets
+                .SingleOrDefault(pet => pet.id == id); 
+
+            if(pet is null) {
+                return NotFound(); // res.sendStatus(404)
+            }
+
+            return pet;
+        }
+
+        // --- Post New Pet ----------------------------------------------------
+        [HttpPost]
+        public IActionResult Post(Pet pet)
+        {
+            _context.Add(pet); // This posts the pet to the database
+            _context.SaveChanges(); // This commits the change
+            
+            return CreatedAtAction(nameof(Post), new { id = pet.id }, pet); // This returns the added pet's information
+        }
+
+        // --- Delete Pet ------------------------------------------------------
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Pet pet = _context.Pets.SingleOrDefault(p => p.id == id); // I believe this checks if the pet exists or not
+
+            if(pet is null){
+                return NotFound(); // If the pet doesn't exist, it returns 404 Not Found error
+            }
+            
+            _context.Pets.Remove(pet); // This deletes the pet
+            _context.SaveChanges(); // This commits the change
+
+            // 204
+            return NoContent(); // This returns a confirmation of deletion
+        }
+        
+        // --- Update Pet ------------------------------------------------------
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Pet pet)
+        {
+            Console.WriteLine("in PUT pet");
+            if (id != pet.id)
+            {
+                return BadRequest();
+            }
+            // update in DB
+            _context.Update(pet);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+
+
+
+
+
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
-        [HttpGet]
-        public IEnumerable<Pet> GetPets() {
-            return new List<Pet>();
-        }
+        
+        // [HttpGet]
+        // public IEnumerable<Pet> GetPets() {
+        //     return new List<Pet>();
+        // }
 
         // [HttpGet]
         // [Route("test")]
