50d585abb8a05fe4d0a617fd224340800c026978
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-31 11:26:04 -0500

    created PetOwner controller

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,15 +12,45 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
-            return new List<PetOwner>();
+        public IEnumerable<PetOwner> GetPetOwners()
+        { //GetPetOwners is taco
+            Console.WriteLine("get all bakers");
+            // no SQL
+            return _context.PetOwners;
         }
+
+        [HttpPost]
+        public IActionResult Post(PetOwner petOwner)
+        {
+            _context.Add(petOwner);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Post), new { id = petOwner.id }, petOwner);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<PetOwner> GetById(int id) 
+        {
+            Console.WriteLine("get one pet owner");
+            PetOwner petOwner = _context.PetOwners
+                .SingleOrDefault(petOwner => petOwner.id == id); 
+
+            if(petOwner is null) {
+                return NotFound(); // res.sendStatus(404)
+            }
+
+            return petOwner;
+        }
+
+        
     }
 }
